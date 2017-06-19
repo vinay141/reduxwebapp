472a5785393ddd1d99e35b227f4d0257820355a7
vinay.thimmaraju <vinay.thimmaraju@wipro.com>
2017-06-19 10:42:56 +0100

    redux commits

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import WeatherReducer from '../reducers/reducer_weather';
+
+const rootReducer = combineReducers({
+  weather: WeatherReducer
+});
+
+export default rootReducer;
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,15 @@
+import  { FETCH_WEATHER } from '../actions/index';
+
+export default function(state, action) {
+
+    if (typeof state === 'undefined') {
+        state = [];
+    }
+
+    switch(action.type) {
+        case FETCH_WEATHER:
+            return [ action.payload.data, ...state ];
+    }
+
+    return state;
+}
